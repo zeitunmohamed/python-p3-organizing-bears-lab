574dbe58173a836a7729862266122a1e34f4bdb3
zeitunmohamed <zeitunmohamed@gmail.com>
2025-07-02 09:30:09 +0300

    complete lab

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS bears;
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+  ('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+  ('Tabitha', 4, 'F', 'Golden', 'Playful', 1),
+  ('Sergeant Brown', 8, 'M', 'Brown', 'Serious', 1),
+  ('Melissa', 6, 'F', 'White', 'Gentle', 1),
+  ('Grinch', 7, 'M', 'Green', 'Grumpy', 0),
+  ('Wendy', 3, 'F', 'Grey', 'Happy', 1),
+  (NULL, 2, 'F', 'Black', 'Curious', 1);
